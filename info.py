01943e720199854755f8480a20345bbc93f80d00
Nguyen, Benson <benson-nguyen@uiowa.edu>
2019-10-12 16:12:33 -0500

    finished with credit card info

--- info.py
+++ info.py
@@ -19,9 +19,50 @@ def name():
     firstName = 'Tony Stark'
 
 def cardNumber():
-    creditCard = raw_input("Enter your credit card number : ")
+    creditCard = int(raw_input("Enter your credit card number : "))
     theNumber = []
     theNumber.append(creditCard)
-    print(theNumber)
+    creditCardNumber = theNumber[0]
+    lengthOfNum = len(str(creditCardNumber))
+    if(lengthOfNum != 16 | lengthOfNum != 19):
+        print('invalid credit card number')
+        cardNumber()
+    print(creditCardNumber)
+
+
+def cardExpiration():
+    expirationNum = int(raw_input("Enter expiration number in MMYY format :"))
+    theNumber = []
+    theNumber.append(expirationNum)
+    numbers = theNumber[0]
+    lengthOfNum = len(str(numbers))
+    if(lengthOfNum != 4):
+        print('invalid format, please retry again')
+        cardExpiration()
+    print(numbers)
+
+def cvc():
+    cvcNum = int(raw_input("Enter the cvc number :"))
+    theNumber = []
+    theNumber.append(cvcNum)
+    numbers = theNumber[0]
+    lengthOfNum = len(str(numbers))
+    if(lengthOfNum != 3 | lengthOfNum != 4):
+        print('invalid cvc number, try again')
+        cvc()
+    print(numbers)
+
+
+def zip():
+    zipNum = int(raw_input("Enter zipcode :"))
+    theNumber = []
+    theNumber.append(zipNum)
+    numbers = theNumber[0]
+    lengthOfNum = len(str(numbers))
+    if (lengthOfNum != 5):
+        print('invalid zipcode, try again')
+        zip()
+    print(zip)
+
 if __name__ == '__main__':
-    print(cardNumber())
+    cardExpiration()
